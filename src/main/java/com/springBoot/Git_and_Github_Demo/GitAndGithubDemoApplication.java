5cb07e19746940452547cfcef5d8792b8ed953df
Muneebullah <113171090+muneebullahnoori@users.noreply.github.com>
2024-05-01 15:23:09 +0430

    added project to the git Local Repository

---
+++ src/main/java/com/springBoot/Git_and_Github_Demo/GitAndGithubDemoApplication.java
@@ -0,0 +1,12 @@
+package com.springBoot.Git_and_Github_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAndGithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAndGithubDemoApplication.class, args);
+	}
+}
